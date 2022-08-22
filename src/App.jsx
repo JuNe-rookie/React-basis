391d9007c773b85f1a1245e2d4966618da931f55
liuyue <liuyue@soyoung.com>
2022-08-22 14:46:21 +0800

    清空

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
-import useWindowScroll from "./hooks/useWindowScroll"
-import useLocalStorange from "./hooks/useLocalStorange"
-
-
 function App() {
-	const [y] = useWindowScroll()
-	const [message, setMessage] = useLocalStorange('hook-key', 'jojo')
-	setTimeout(() => {
-		setMessage("111")
-	}, 5000)
 	return (
-		<div style={{ height: "1200px" }}>
-			<div>{y}</div>
-			<div>{message}</div>
-		</div>
+		<div>this is App</div>
 	)
 }
+
 export default App
